a730abb14bfb85a8254311d76ddc3a3312b1f5e5
Lance Newman <lance.newman@elynxx.com>
2018-04-11 16:15:07 -0400

    added conference dto classes

--- BackEnd/Models/Speaker.cs
+++ BackEnd/Models/Speaker.cs
@@ -1,19 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace BackEnd.Models
+﻿namespace BackEnd.Models
 {
-    public class Speaker
+    public class Speaker : ConferenceDTO.Speaker
     {
-        public int ID { get; set; }
-
-        [Required]
-        [StringLength(200)]
-        public string Name { get; set; }
-
-        [StringLength(4000)]
-        public string Bio { get; set; }
-
-        [StringLength(1000)]
-        public virtual string WebSite { get; set; }
     }
 }
